e8d3a069036888a8c508eebdb38b4984c64216f6
Aaron-2792 <aaron92727@hotmail.com>
2025-03-01 15:29:17 -0600

    updated api

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-var http = require('http');
-var url = require('url');
-var dt = require('./datetime');
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
+// Enable CORS for the specific client URL
+app.use(cors({
+    origin: 'https://nice-ocean-0f4f77210.4.azurestaticapps.net'  // Allow the dice roller client domain
+}));
 
-    // Standard Hello World.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Hello World!</h3>')
-
-    // Access funcion from a separate JavaScript module.
-    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
-
-    // Show the url. 
-    response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something tl the url so that we can parse it. 
-    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
-    var q = url.parse(request.url, true).query;
-    var txt = q.year + " " + q.month;
-    response.write("txt="+txt);
-
-    // Close the response
-    response.end('<h3>The End.</h3>');
+// RESTful API endpoint for generating a dice roll
+app.get('/api/roll-dice', (req, res) => {
+    const diceRoll = Math.floor(Math.random() * 6) + 1; // 6-sided die roll
+    res.json({ roll: diceRoll });
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
